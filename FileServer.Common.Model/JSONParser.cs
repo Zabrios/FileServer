bfd8db6c47843532f74486a5fbd188800462494c
Daniel Ruz <daniel.ruzvaz@gmail.com>
2018-06-27 17:19:46 +0200

    Added jsonparser prototype

---
+++ FileServer.Common.Model/JSONParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileServer.Common.Model
+{
+    public class JSONParser
+    {
+        public void CreateJSONFile(string path)
+        {
+            StreamWriter file = new StreamWriter(path, true);
+            if (!JSONFileExists(path))
+            {
+                file = File.CreateText(path);
+            }
+        }
+
+        public bool JSONFileExists(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}
