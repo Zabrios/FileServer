b7a5df1117048f981dc11c908f053918779d1d88
Daniel Ruz <daniel.ruzvaz@gmail.com>
2018-07-06 15:46:25 +0200

    Changed FileManager interface

--- FileServer.Infrastucture.Repository/Interfaces/IFileManager.cs
+++ FileServer.Infrastucture.Repository/Interfaces/IFileManager.cs
@@ -5,5 +5,7 @@
         void CreateJSONFileIfNonexistent(string path);
         bool JSONFileExists(string path);
         string PathSelector(int comboIndex);
+        void WriteToJson(string path, string jsonData);
+        string RetrieveJSONData(string path);
     }
 }
